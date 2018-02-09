8f35e8ee7aa811b9965b2ca8f1725ab72f592a83
mmulla4 <mmulla4@uic.edu>
2018-02-08 23:40:27 -0600

    Cleaned unused files

--- src/main/java/com/zappos/restaurants/controller/WelcomeRestController.java
+++ src/main/java/com/zappos/restaurants/controller/WelcomeRestController.java
@@ -13,9 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeRestController {
 	
 	
-	
-	
-	
 	@RequestMapping(value="/",method=RequestMethod.GET,headers="Accept=application/json",produces="application/json")
 	public String getGreetingHello(){
 		String availableEndpoints = "Welcome to Restaurant Selector - Available Endpoints \n"+
@@ -24,15 +21,10 @@ public class WelcomeRestController {
 									"3. /addRestaurant - Adds a Single Restaurant to database \n"+
 									"4. /removeRestaurant/{id} - Removes Restaurant having {id} \n"+
 									"5. /getMenuTypeByRestaurantId/{restId} - Returns all available menu types for restaurant \n"+
-									"6. /addMenuTypeForRestaurant - Add a menu type with 0 menu item \n"+
-									"7. /deleteMenuTypeForRestaurant/{restId} - Deletes a menu type for a restaurant \n"+
-									"8. /addMenuItemForMenu/{menuId} -Add a menu Item for a specific menu type \n"+
-									"9. /getMenuItemsForMenu/{menuId} - Returns all Menu Item for a Menu Type \n"+
-									"10. /deleteMenuItemForMenu/{menuId}- Deletes a Menu Item\n";
+									"6. /addMenuTypeForRestaurant - Add a menu type with 0 menu item \n";
 		
 		return availableEndpoints; 
-		
-		
+			
 	}
 
 }
